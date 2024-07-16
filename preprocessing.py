ba5628d5ae7646e14af1c5183aa4db3ae32575f0
Kyle-Brennan <kdogian401@gmail.com>
2024-07-15 21:12:21 -0400

    Proper noun extraction
    For finger spelling

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,6 @@
 import csv
 import nltk.tokenize as tk
+import en_core_web_sm
 
 from nltk.stem import WordNetLemmatizer
 from string import punctuation
@@ -7,19 +8,27 @@ from string import punctuation
 
 def main():
     lemmatizer = WordNetLemmatizer()
-    stopwords = []
 
     try:
         # Collect all stopwords from asl_stopwords.csv; add to it at any time
         with open("asl_stopwords.csv", newline="") as sw_file:
+            stopwords = []
             for word in csv.reader(sw_file):
                 stopwords.append(word[0])
-        # Convert text file to list of strings
+        # Convert transcription text file to a string
         with open("transcription.txt", "r") as txt_obj:
             text = txt_obj.read()
             txt_obj.close()
     except FileNotFoundError:
         pass
+
+    # Proper noun extraction
+    nlp = en_core_web_sm.load()
+    proper_nouns = []
+    for word in nlp(text):
+        if word.pos_ == 'PROPN':
+            proper_nouns.append(word.text)
+    print(proper_nouns)
 
     # Create tokenized sentences from transcription
     sentences = tk.sent_tokenize(text)
@@ -32,7 +41,6 @@ def main():
             token = words[i][j].lower()
             if token not in punctuation and token not in stopwords:
                 tokenized_words[i].append(token)
-
 
 
 if __name__ == "__main__":
